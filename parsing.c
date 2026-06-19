236f172411ba8a913ee21b589834d68299ebe682
guantino <guantino@student.42malaga.com>
2026-06-19 16:08:44 +0200

    a

--- parsing.c
+++ parsing.c
@@ -31,7 +31,8 @@ static int	ft_is_valid_scheduler(const char *s)
 static char *ft_strncpy(char *dest, const char *src, size_t n)
 {
     size_t i;
-
+    
+    i = 0;
     while (i < n && src[i] != '\0')
     {
         dest[i] = src[i];
